60112fcbde34ab94c90a9dec09a0198ad31f923a
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2025-03-15 12:12:12 +0200

    Add terraform dags

---
+++ 02_workflow_orchestration/dags/terraform_destroy_infra.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+# Define the DAG
+dag = DAG(
+    'terraform_destroy_infra',
+    description = 'Destroy GCP infrastructure with Terraform',
+    schedule_interval = None,
+    start_date = datetime(2025, 3, 14),
+    catchup=False
+)
+
+terraform_destroy = BashOperator(
+    task_id = 'terraform_destroy',
+    bash_command = '/home/airflow/scripts/terraform/terraform_destroy.sh ',
+    dag=dag
+)
+
+
+terraform_destroy
---
+++ 02_workflow_orchestration/dags/terraform_raise_infra.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+# Define the DAG
+dag = DAG(
+    'terraform_raise_infra',
+    description = 'Raise GCP infrastructure with Terraform',
+    schedule_interval = None,
+    start_date = datetime(2025, 3, 14),
+    catchup=False
+)
+
+terraform_init = BashOperator(
+    task_id = 'terraform_init',
+    bash_command = '/home/airflow/scripts/terraform/terraform_init.sh ',
+    dag=dag
+)
+
+terraform_apply = BashOperator(
+    task_id = 'terraform_apply',
+    bash_command =' /home/airflow/scripts/terraform/terraform_apply.sh ',
+    dag = dag
+)
+
+terraform_init >> terraform_apply
