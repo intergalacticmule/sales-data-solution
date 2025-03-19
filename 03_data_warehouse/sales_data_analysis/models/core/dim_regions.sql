263040e5e30a58fc76565dbc88c90a3f97c775d8
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2025-03-19 22:25:50 +0200

    Add dim_regions model

---
+++ 03_data_warehouse/sales_data_analysis/models/core/dim_regions.sql
@@ -0,0 +1,16 @@
+{{
+  config(
+    materialized = 'table'
+    )
+}}
+
+with regions as (
+  select 
+    distinct(Region) as region_name
+  from {{ ref('st_sales_data') }}
+)
+
+select
+  row_number() over ()as region_id,
+  region_name
+from regions
