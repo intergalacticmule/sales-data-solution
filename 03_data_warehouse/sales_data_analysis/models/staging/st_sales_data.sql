48e3e77260ec01d5a4decd9ca5e6a21ca4ce2e84
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2025-03-19 21:47:15 +0200

    Remove persist_docs

--- 03_data_warehouse/sales_data_analysis/models/staging/st_sales_data.sql
+++ 03_data_warehouse/sales_data_analysis/models/staging/st_sales_data.sql
@@ -1,12 +1,11 @@
 {{
     config(
-        materialized='table',
-        partition_by={
+        materialized = 'table',
+        partition_by = {
             "field": "Sale_Date",
             "data_type": "date",
             "granularity": "day"
-        },
-        persist_docs={"columns": true}
+        }
     )
 }}
 
