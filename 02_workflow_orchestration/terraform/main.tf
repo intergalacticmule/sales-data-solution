00d02c2f07456f4282f8abcfaa86e81d1ed9025d
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2025-03-19 16:07:38 +0200

    Add delete_contents_on_destroy = true

--- 02_workflow_orchestration/terraform/main.tf
+++ 02_workflow_orchestration/terraform/main.tf
@@ -31,4 +31,5 @@ resource "google_storage_bucket" "sales-data-analysis-bucket" {
 resource "google_bigquery_dataset" "sales-data-analysis-dataset" {
   dataset_id = var.bq_dataset_name
   location   = var.location
+  delete_contents_on_destroy = true
 }
