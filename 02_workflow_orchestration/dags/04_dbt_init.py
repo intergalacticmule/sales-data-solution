37c3254e759411641780eac899a9dfda2ccafcae
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2025-03-20 09:26:22 +0200

    Add dbt deps to dag

--- 02_workflow_orchestration/dags/04_dbt_init.py
+++ 02_workflow_orchestration/dags/04_dbt_init.py
@@ -3,8 +3,8 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
 with DAG(
-    "04_dbt_test_connection",
-    description = "Test dbt connection to BigQuery",
+    "04_dbt_init",
+    description = "dbt test connection and install deps",
     schedule_interval = None,
     start_date = datetime(2025, 3, 14),
     catchup=False
@@ -15,4 +15,9 @@ with DAG(
         bash_command =" /home/airflow/scripts/dbt/debug.sh "
     )
 
-dbt_debug
+    dbt_deps = BashOperator(
+        task_id = "dbt_deps",
+        bash_command =" /home/airflow/scripts/dbt/deps.sh "
+    )
+
+dbt_debug >> dbt_deps
