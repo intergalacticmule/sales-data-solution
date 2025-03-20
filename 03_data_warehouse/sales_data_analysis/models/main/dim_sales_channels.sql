0c6ddd587db708e0a2ab44ccd3512e21ddec327c
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2025-03-20 13:32:40 +0200

    Add sales channels dimension

---
+++ 03_data_warehouse/sales_data_analysis/models/main/dim_sales_channels.sql
@@ -0,0 +1,17 @@
+{{
+  config(
+    materialized = 'table'
+    )
+}}
+
+with sales_channels as (
+  select 
+    distinct(Sales_Channel) as sales_channel_name
+  from {{ ref('st_sales_data') }}
+)
+
+select
+  row_number() over ()as sales_channel_id,
+  sales_channel_name
+from sales_channels
+order by 1
