225374af699a7bae39391bb757bac1c2fce2a6cd
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2025-03-21 11:41:18 +0200

    Update dag name

--- 02_workflow_orchestration/dags/07_terraform_destroy_infra.py
+++ 02_workflow_orchestration/dags/07_terraform_destroy_infra.py
@@ -3,7 +3,7 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
 with DAG(
-    "terraform_destroy_infra",
+    "07_terraform_destroy_infra",
     description = "Destroy GCP infrastructure with Terraform",
     schedule_interval = None,
     start_date = datetime(2025, 3, 14),
